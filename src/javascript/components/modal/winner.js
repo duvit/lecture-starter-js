c608d5dc66e1a22bd11b8ff7afd02381377591e5
Vitalii Dutov <dutov.vit.vic@gmail.com>
2024-06-07 07:23:32 +0300

    final

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -3,11 +3,12 @@ import createElement from '../../helpers/domHelper';
 
 export default function showWinnerModal(fighter) {
     const title = 'Fight Result';
-    const bodyElement = createElement({
-        tagName: 'div',
-        className: 'modal-body'
-    });
+    const bodyElement = createElement({ tagName: 'div', className: 'modal-body' });
     bodyElement.textContent = `${fighter.name} is the winner!`;
 
+    const imageElement = createElement({ tagName: 'img', attributes: { src: fighter.source } });
+    bodyElement.append(imageElement);
+
     showModal({ title, bodyElement });
+
 }
